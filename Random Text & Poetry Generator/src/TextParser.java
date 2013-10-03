38a993afbffc87e0e73ba20d582a1d8569ee9c90
Mane Williams <Mane@Mane-Williamss-MacBook-Pro.local>
2013-10-03 08:26:01 -0700

    example markov chain poem (T. S. Elliot)

--- Random Text & Poetry Generator/src/TextParser.java
+++ Random Text & Poetry Generator/src/TextParser.java
@@ -6,5 +6,13 @@ public class TextParser {
 		//Creates a new TextParser that will read from the file specified by path.
 		
 	}
-	
+	/*
+	Under the brown land, mixing
+	Winter
+	With a burnished throne
+	Burned green and gold.
+	Where the faint moonlight, the chapel, only
+	Fishing in soda water
+	Endeavors to folly and puts a fortnight dead mountain mouth (\n?) of empty rooms
+	*/
 }
