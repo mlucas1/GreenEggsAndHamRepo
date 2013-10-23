d4ec604e94e701c73723e466d11966645cbdc6f1
mlucas1 <mlucas1@hwemail.com>
2013-10-23 13:37:28 -0700

    changed tester files

--- Random Text & Poetry Generator/src/MarkovTester.java
+++ Random Text & Poetry Generator/src/MarkovTester.java
@@ -5,9 +5,9 @@ import javax.swing.JOptionPane;
 
 public class MarkovTester {
 	public static void main(String[] args) throws IOException {
-		InputStream inOne=new BufferedInputStream(new FileInputStream(new File("arrays/Eliot.txt")));
-		InputStream inTwo=new BufferedInputStream(new FileInputStream(new File("arrays/Eliot.txt")));
-		TextParser parser=new TextParser(inOne, inTwo, false);
+		InputStream inOne=new BufferedInputStream(new FileInputStream(new File("emerson.txt")));
+		InputStream inTwo=new BufferedInputStream(new FileInputStream(new File("emerson.txt")));
+		TextParser parser=new TextParser(inOne, inTwo, true);
 		TextGenerator generator=new TextGenerator(parser);
 		String result = generator.generateText(30);
 		//generator.readAloud(result);
