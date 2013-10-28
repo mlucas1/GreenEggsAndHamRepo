b0823834c301863ad03ce3e681e76632c0bd1026
mlucas1 <mlucas1@hwemail.com>
2013-10-28 13:51:39 -0700

    Added Moby-Dick, MarkovTester scrolling text

--- Random Text & Poetry Generator/src/MarkovTester.java
+++ Random Text & Poetry Generator/src/MarkovTester.java
@@ -1,20 +1,32 @@
+import java.awt.Dimension;
 import java.io.*;
 
 import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 
 public class MarkovTester {
 	public static void main(String[] args) throws IOException {
-		InputStream inOne=new BufferedInputStream(new FileInputStream(new File("Whitman.txt")));
-		InputStream inTwo=new BufferedInputStream(new FileInputStream(new File("Whitman.txt")));
+		InputStream inOne=new BufferedInputStream(new FileInputStream(new File("Melville.txt")));
+		InputStream inTwo=new BufferedInputStream(new FileInputStream(new File("Melville.txt")));
 		TextParser parser=new TextParser(inOne, inTwo, true);
 		TextGenerator generator=new TextGenerator(parser);
-		String result = generator.generateText(30);
+		String result = generator.generateText(2);
+		
+		JTextArea msg = new JTextArea(result);
+		msg.setLineWrap(true);
+		msg.setWrapStyleWord(true);
+
+		JScrollPane scrollPane = new JScrollPane(msg);
+		scrollPane.setPreferredSize(new Dimension(800,500)); 
+
 		//generator.readAloud(result);
 		System.out.println("Total Words: " + parser.numWords);
 		System.out.println("Unique Words: " + parser.numUniqueTokens);
 		System.out.println(Runtime.getRuntime().maxMemory() - Runtime.getRuntime().freeMemory());
-		JOptionPane.showMessageDialog(null, "The result is: \n"+result);
+		
+		JOptionPane.showMessageDialog(null, scrollPane);
 		
 	}
 }
